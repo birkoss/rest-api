da252412c5053c3b31b8af576355b6cceff9eb47
Mathieu Robichaud <admin@birkoss.com>
2024-11-26 19:04:15 -0500

    Cleanup

--- api/model.php
+++ api/model.php
@@ -5,17 +5,4 @@ namespace Api\Model;
 class Model extends \Api\Database\Database {
     public $writable_fields = array();
     public $readable_fields = array();
-
-
-    public function filters($data) {
-        foreach ($data as $index => $single_data) {
-            $data[$index] = $this->filter($single_data);
-        }
-        return $data;
-    }
-
-    
-    public function filter($fields) {
-        return array_intersect_key($fields, array_flip($this->readable_fields));
-    }
 }
